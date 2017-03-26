3fc82d3097b9403b251b0d3c48d8e932b45d8325
Andy Butland <abutland@thisiszone.com>
2017-03-26 20:57:16 +0200

    Initial commit

---
+++ UmbracoFileSystemProviders.RackspaceCloudFiles.IntegrationTests/CloudFilesFileSystemTests.cs
@@ -0,0 +1,199 @@
+﻿namespace UmbracoFileSystemProviders.RackspaceCloudFiles.IntegrationTests
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Umbraco.Core.IO;
+
+    [TestClass]
+    [DeploymentItem("TestFile\flower.jpg")]
+    [DeploymentItem("TestFile\flower2.jpg")]
+    public class CloudFilesFileSystemTests
+    {
+        private readonly string _apiKey = ConfigurationManager.AppSettings["ApiKey"];
+        private readonly string _username = ConfigurationManager.AppSettings["Username"];
+        private readonly string _container = ConfigurationManager.AppSettings["Container"];
+
+        private const string TestFile1 = "TestFile\\flower.jpg";
+        private const string TestFile2 = "TestFile\\flower2.jpg";
+
+        [TestMethod]
+        public void GetDirectoriesTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var path1 = "1010/flower.jpg";
+            var path2 = "1011/flower2.jpg";
+            EnsureTestFileAdded(fs, path1);
+            EnsureTestFileAdded(fs, path2);
+
+            // Act
+            var result = fs.GetDirectories("/").ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("1010", result.First());
+            Assert.AreEqual("1011", result.Last());
+        }
+
+        [TestMethod]
+        public void DeleteDirectoryTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var directory = "1010/";
+            var path1 = "1010/flower.jpg";
+            var path2 = "1010/flower2.jpg";
+            EnsureTestFileAdded(fs, path1);
+            EnsureTestFileAdded(fs, path2);
+
+            // Act
+            fs.DeleteDirectory(directory);
+
+            // Assert
+            Assert.IsFalse(fs.DirectoryExists(directory));
+        }
+
+        [TestMethod]
+        public void DirectoryExistsTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var directory = "1010/";
+            var path1 = "1010/flower.jpg";
+            EnsureTestFileAdded(fs, path1);
+
+            // Act
+            var result = fs.DirectoryExists(directory);
+            var result2 = fs.DirectoryExists("notthere/");
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.IsFalse(result2);
+        }
+
+        [TestMethod]
+        public void AddFileTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var path = "1010/flower.jpg";
+
+            // Act
+            EnsureTestFileAdded(fs, path);
+
+            // Assert
+            Assert.IsTrue(fs.FileExists(path));
+        }
+
+        [TestMethod]
+        public void OpenFileTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var path = "1010/flower.jpg";
+            EnsureTestFileAdded(fs, path);
+
+            // Act
+            var result = fs.OpenFile(path);
+
+            // Assert
+            var testFilePath = Path.Combine(Environment.CurrentDirectory, TestFile1);
+            long expectedLength;
+            using (var stream = File.Open(testFilePath, FileMode.Open))
+            {
+                expectedLength = stream.Length;
+            }
+
+            Assert.AreEqual(expectedLength, result.Length);
+        }
+
+        [TestMethod]
+        public void DeleteFileTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var path = "1010/flower.jpg";
+            EnsureTestFileAdded(fs, path);
+
+            // Act
+            fs.DeleteFile(path);
+
+            // Assert
+            Assert.IsFalse(fs.FileExists(path));
+        }
+
+        [TestMethod]
+        public void GetFilesTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var directory = "1010/";
+            var path1 = "1010/flower.jpg";
+            var path2 = "1010/flower2.jpg";
+            var path3 = "1011/flower.jpg";
+            EnsureTestFileAdded(fs, path1);
+            EnsureTestFileAdded(fs, path2);
+            EnsureTestFileAdded(fs, path3);
+
+            // Act
+            var result = fs.GetFiles(directory).ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("1010/flower.jpg", result.First());
+            Assert.AreEqual("1010/flower2.jpg", result.Last());
+        }
+
+        [TestMethod]
+        public void FileExistsTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var path = "1010/flower.jpg";
+            EnsureTestFileAdded(fs, path);
+
+            // Act
+            var result = fs.FileExists(path);
+            var result2 = fs.FileExists(path.Replace("flower", "notthere"));
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.IsFalse(result2);
+        }
+
+        [TestMethod]
+        public void GetLastModifiedTest()
+        {
+            // Arrange
+            var fs = CreateFileSystem();
+            var path = "1010/flower.jpg";
+            EnsureTestFileAdded(fs, path);
+
+            // Act
+            var result = fs.GetLastModified(path);
+
+            // Assert
+            var today = DateTime.Now;
+            Assert.AreEqual(today.Year, result.Year);
+            Assert.AreEqual(today.Month, result.Month);
+            Assert.AreEqual(today.Day, result.Day);
+        }
+
+        private IFileSystem CreateFileSystem()
+        {
+            return new CloudFilesFileSystem(_apiKey, _username, _container);
+        }
+
+        private static void EnsureTestFileAdded(IFileSystem fs, string path)
+        {
+            var testFilePath = Path.Combine(Environment.CurrentDirectory, TestFile1);
+            using (var stream = File.Open(testFilePath, FileMode.Open))
+            {
+                fs.AddFile(path, stream, true);
+            }
+        }
+    }
+}
---
+++ UmbracoFileSystemProviders.RackspaceCloudFiles/CloudFilesFileSystem.cs
@@ -0,0 +1,193 @@
+﻿namespace UmbracoFileSystemProviders.RackspaceCloudFiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using net.openstack.Core.Domain;
+    using net.openstack.Core.Exceptions.Response;
+    using net.openstack.Providers.Rackspace;
+    using Umbraco.Core.IO;
+
+    public class CloudFilesFileSystem : IFileSystem
+    {
+        private readonly string _apiKey;
+        private readonly string _username;
+        private readonly string _container;
+
+        public CloudFilesFileSystem(string apiKey, string username, string container)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new ArgumentNullException(nameof(apiKey), "apiKey cannot be null");
+            }
+
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException(nameof(username), "username cannot be null");
+            }
+
+            if (string.IsNullOrEmpty(container))
+            {
+                throw new ArgumentNullException(nameof(container), "containerName cannot be null");
+            }
+
+            _apiKey = apiKey;
+            _username = username;
+            _container = container;
+        }
+
+        public IEnumerable<string> GetDirectories(string path)
+        {
+            if (path == null)
+            {
+                path = string.Empty;
+            }
+
+            if (path.StartsWith("/"))
+            {
+                path = path.Substring(1);
+            }
+
+            var provider = GetProvider();
+            return provider.ListObjects(_container, prefix: path)
+                .Select(x => x.Name.Split('/').First())
+                .Distinct();
+        }
+
+        public void DeleteDirectory(string path)
+        {
+            DeleteDirectory(path, false);
+
+        }
+
+        public void DeleteDirectory(string path, bool recursive)
+        {
+            var provider = GetProvider();
+            var directoryContents = provider.ListObjects(_container, prefix: path)
+                .Select(x => x.Name)
+                .ToList();
+
+            foreach (var directoryPath in directoryContents)
+            {
+                provider.DeleteObject(_container, directoryPath);
+            }
+
+            // Delete "directory" itself (fail silently if already removed)
+            if (path.EndsWith("/"))
+            {
+                path = path.Substring(0, path.Length - 1);
+            }
+
+            try
+            {
+                provider.DeleteObject(_container, path);
+            }
+            catch (ItemNotFoundException)
+            {
+            }
+        }
+
+        public bool DirectoryExists(string path)
+        {
+            var provider = GetProvider();
+            return provider.ListObjects(_container, prefix: path)
+                .Any();
+        }
+
+        public void AddFile(string path, Stream stream)
+        {
+            AddFile(path, stream, true);
+        }
+
+        public void AddFile(string path, Stream stream, bool overrideIfExists)
+        {
+            var provider = GetProvider();
+            if (overrideIfExists || FileExists(path) == false)
+            {
+                provider.CreateObject(_container, stream, path);
+            }
+        }
+
+        public IEnumerable<string> GetFiles(string path)
+        {
+            return GetFiles(path, string.Empty);
+        }
+
+        public IEnumerable<string> GetFiles(string path, string filter)
+        {
+            var provider = GetProvider();
+            return provider.ListObjects(_container, prefix: path)
+                .Select(x => x.Name);
+        }
+
+        public Stream OpenFile(string path)
+        {
+            var provider = GetProvider();
+            var stream = new MemoryStream();
+            provider.GetObject(_container, path, stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            return stream;
+        }
+
+        public void DeleteFile(string path)
+        {
+            var provider = GetProvider();
+            provider.DeleteObject(_container, path);
+        }
+
+        public bool FileExists(string path)
+        {
+            var provider = GetProvider();
+            try
+            {
+                provider.GetObjectHeaders(_container, path);
+                return true;
+            }
+            catch (ItemNotFoundException)
+            {
+                return false;
+            }
+        }
+
+        public string GetRelativePath(string fullPathOrUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFullPath(string path)
+        {
+            return path;
+        }
+
+        public string GetUrl(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTimeOffset GetLastModified(string path)
+        {
+            var provider = GetProvider();
+            var headers = provider.GetObjectHeaders(_container, path);
+            var lastModifiedHeader = headers["Last-Modified"];
+            return DateTimeOffset.Parse(lastModifiedHeader);
+        }
+
+        public DateTimeOffset GetCreated(string path)
+        {
+            // No header for created, so return last modifiled
+            return GetLastModified(path);
+        }
+
+        private CloudFilesProvider GetProvider()
+        {
+            var identity = new CloudIdentity
+            {
+                APIKey = _apiKey,
+                Username = _username
+            };
+
+            return new CloudFilesProvider(identity);
+        }
+    }
+}
